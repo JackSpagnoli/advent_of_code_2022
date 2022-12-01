f6b3a6f44a80eeaa2c1d27eda6761465477d32f1
jasp <jackspagnoli@gmail.com>
2022-12-01 09:21:51 +0000

    solves day01 part 1

--- Day01/src/main.rs
+++ Day01/src/main.rs
@@ -1,3 +1,19 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let contents = fs::read_to_string("input.txt").expect("Error reading file");
+    let split_contents = contents.lines();
+    let mut max: u32 = 0;
+    let mut current: u32 = 0;
+    for line in split_contents {
+        if line == "" {
+            if current > max {
+                max = current;
+            }
+            current = 0;
+        } else {
+            current += line.parse::<u32>().unwrap();
+        }
+    }
+    println!("{max}");
 }
