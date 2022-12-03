05f3701bc66a895909b67a96ad11a41c5c1cfc18
jasp <jackspagnoli@gmail.com>
2022-12-03 11:07:55 +0000

    completes part 2

--- Day03/src/main.rs
+++ Day03/src/main.rs
@@ -4,6 +4,8 @@ use substring::Substring;
 fn main() {
     assert!(sum_priorities("test_input.txt") == 157);
     println!("{}", sum_priorities("input.txt"));
+    assert!(sum_group_priorities("test_input.txt") == 70);
+    println!("{}", sum_group_priorities("input.txt"));
 }
 
 fn sum_priorities(file: &str) -> u32 {
@@ -46,4 +48,46 @@ fn sum_priorities(file: &str) -> u32 {
         return n + (score_change as u32);
     });
     return score;
+}
+
+fn sum_group_priorities(file: &str) -> u32 {
+    let contents = fs::read_to_string(file).expect("Error reading file");
+    let mut split_contents = contents.lines();
+
+    let total_groups = split_contents.clone().count() / 3;
+
+    let mut score: usize = 0;
+
+    for _group in 0..total_groups {
+        let mut lines_contents: [[u32; 52]; 3] = [[0; 52]; 3];
+        let mut lines = vec![
+            split_contents.next().unwrap().chars(),
+            split_contents.next().unwrap().chars(),
+            split_contents.next().unwrap().chars()
+        ];
+
+        for i in 0..3 {
+            while lines[i].size_hint().0 > 0 {
+                let mut character = (lines[i].next().unwrap() as usize) - 64;
+                if character > 26 {
+                    character -= 32;
+                } else {
+                    character += 26;
+                }
+
+                lines_contents[i][character - 1] += 1;
+            }
+        }
+
+        for i in 0..52 {
+            if lines_contents[0][i] > 0 {
+                if lines_contents[1][i] > 0 {
+                    if lines_contents[2][i] > 0 {
+                        score += i + 1;
+                    }
+                }
+            }
+        }
+    }
+    return score as u32;
 }
