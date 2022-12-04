87dff50df2cf9a2bdcb91323dce19671b2583025
jasp <jackspagnoli@gmail.com>
2022-12-04 10:24:00 +0000

    completes part 1

---
+++ day04/src/main.rs
@@ -0,0 +1,30 @@
+use std::fs;
+use std::str::FromStr;
+
+fn main() {
+    assert!(overlapping_pairs("test_input.txt") == 2);
+    println!("{}", overlapping_pairs("input.txt"));
+}
+
+fn overlapping_pairs(file: &str) -> u32 {
+    let contents = fs::read_to_string(file).expect("Error reading file");
+    let split_contents = contents.lines();
+
+    return split_contents.fold(0, |n, x| {
+        let mut split_line = x.split(",");
+
+        let mut left_range = split_line.next().unwrap().split("-");
+        let left_a = <u32 as FromStr>::from_str(left_range.next().unwrap()).unwrap() as u32;
+        let left_b = <u32 as FromStr>::from_str(left_range.next().unwrap()).unwrap() as u32;
+
+        let mut right_range = split_line.next().unwrap().split("-");
+        let right_a = <u32 as FromStr>::from_str(right_range.next().unwrap()).unwrap() as u32;
+        let right_b = <u32 as FromStr>::from_str(right_range.next().unwrap()).unwrap() as u32;
+
+        if (left_a >= right_a && left_b <= right_b) || (left_a <= right_a && left_b >= right_b) {
+            return n + 1;
+        } else {
+            return n;
+        }
+    });
+}
