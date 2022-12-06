a22ed553d60194cc2da111583e4953100156aeb2
jasp <jackspagnoli@gmail.com>
2022-12-06 09:22:25 +0000

    completes part 2

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -8,16 +8,23 @@ fn main() {
     assert!(length_to_packet_start("test_4_input.txt") == 10);
     assert!(length_to_packet_start("test_5_input.txt") == 11);
     println!("{}", length_to_packet_start("input.txt"));
+
+    assert!(length_to_distinct_sequence("test_1_input.txt", 14) == 19);
+    assert!(length_to_distinct_sequence("test_2_input.txt", 14) == 23);
+    assert!(length_to_distinct_sequence("test_3_input.txt", 14) == 23);
+    assert!(length_to_distinct_sequence("test_4_input.txt", 14) == 29);
+    assert!(length_to_distinct_sequence("test_5_input.txt", 14) == 26);
+    println!("{}", length_to_distinct_sequence("input.txt", 14));
 }
 
-fn length_to_packet_start(file: &str) -> u32 {
+fn length_to_distinct_sequence(file: &str, length: usize) -> u32 {
     let input_contents = fs::read_to_string(file).expect("Error reading file");
     let binding = input_contents.lines().collect::<Vec<&str>>();
     let mut line = binding.get(0).unwrap().chars();
 
-    let mut i: u32 = 4;
-    while line.size_hint().1.unwrap() >= 4 {
-        let mut next_four = line.clone().take(4).collect::<Vec<char>>();
+    let mut i: u32 = length as u32;
+    while line.size_hint().1.unwrap() >= length {
+        let mut next_four = line.clone().take(length).collect::<Vec<char>>();
 
         let mut seen = HashSet::new();
 
@@ -26,7 +33,7 @@ fn length_to_packet_start(file: &str) -> u32 {
             seen.insert(*x);
             return !is_seen;
         });
-        if next_four.len() == 4 {
+        if next_four.len() == length {
             return i;
         }
 
@@ -34,4 +41,8 @@ fn length_to_packet_start(file: &str) -> u32 {
         line.next();
     }
     return 0;
+}
+
+fn length_to_packet_start(file: &str) -> u32 {
+    return length_to_distinct_sequence(file, 4);
 }
