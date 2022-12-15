ce8fb1f745b28c47c0eef4e2580baaf93fa3c02d
jasp <jackspagnoli@gmail.com>
2022-12-15 16:19:29 +0000

    solves part 2

--- day15/src/main.rs
+++ day15/src/main.rs
@@ -5,6 +5,38 @@ use std::cmp::{ max, min };
 fn main() {
     assert_eq!(beaconless_spaces("test_input.txt", 10), 26);
     println!("{}", beaconless_spaces("input.txt", 2000000));
+
+    assert_eq!(tuning_frequency("test_input.txt", 20), 56000011);
+    println!("{}", tuning_frequency("input.txt", 4_000_000))
+}
+
+fn tuning_frequency(file: &str, range: isize) -> isize {
+    let (sensor_coordinates, _, _) = sensor_coordinate_pairs(file);
+
+    for i in 0..sensor_coordinates.len() {
+        let ((s_y, s_x), distance) = sensor_coordinates[i];
+
+        for step in 0..distance + 2 {
+            let neighbour_coordinates: Vec<(isize, isize)> = vec![
+                (s_y + step - distance - 1, s_x + step),
+                (s_y + step, s_x + distance + 1 - step),
+                (s_y - step + distance + 1, s_x - step),
+                (s_y - step, s_x + step - distance - 1)
+            ];
+            for (y, x) in neighbour_coordinates {
+                if in_range((y, x), range) {
+                    if !has_sensor_in_range((y, x), &sensor_coordinates) {
+                        return 4_000_000 * x + y;
+                    }
+                }
+            }
+        }
+    }
+    return 0;
+}
+
+fn in_range(coordinates: (isize, isize), range: isize) -> bool {
+    coordinates.0 >= 0 && coordinates.0 <= range && coordinates.1 >= 0 && coordinates.1 <= range
 }
 
 fn beaconless_spaces(file: &str, row: usize) -> usize {
@@ -12,12 +44,8 @@ fn beaconless_spaces(file: &str, row: usize) -> usize {
 
     let mut spaces: usize = 0;
     for x in min_x..max_x + 1 {
-        for i in 0..sensor_coordinates.len() {
-            let (coordinates, distance) = sensor_coordinates[i];
-            if l1_distance(coordinates, (row as isize, x)) <= distance {
-                spaces += 1;
-                break;
-            }
+        if has_sensor_in_range((row as isize, x), &sensor_coordinates) {
+            spaces += 1;
         }
     }
 
@@ -66,7 +94,20 @@ fn sensor_coordinate_pairs(
 }
 
 fn l1_distance((y_1, x_1): (isize, isize), (y_2, x_2): (isize, isize)) -> isize {
-    let dy = y_1 - y_2;
-    let dx = x_1 - x_2;
+    let dy: isize = y_1 - y_2;
+    let dx: isize = x_1 - x_2;
     return max(dy, -dy) + max(dx, -dx);
+}
+
+fn has_sensor_in_range(
+    (y, x): (isize, isize),
+    sensor_coordinates: &Vec<((isize, isize), isize)>
+) -> bool {
+    for i in 0..sensor_coordinates.len() {
+        let (coordinates, distance) = sensor_coordinates[i];
+        if l1_distance(coordinates, (y, x)) <= distance {
+            return true;
+        }
+    }
+    return false;
 }
