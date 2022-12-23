57b88ab6fb25d94ac918850046f2f4bfc210a580
jasp <jackspagnoli@gmail.com>
2022-12-23 22:47:21 +0000

    completes part 2

--- day18/src/main.rs
+++ day18/src/main.rs
@@ -6,11 +6,17 @@ fn surface_area_correctly_calculated() {
     assert_eq!(surface_area("test_input.txt"), 64);
 }
 
-fn main() {
-    println!("{}", surface_area("input.txt"));
+#[test]
+fn interior_correctly_filled() {
+    assert_eq!(exterior_surface_area("test_input.txt"), 58);
 }
 
-fn surface_area(file: &str) -> usize {
+fn main() {
+    println!("Part 1: {}", surface_area("input.txt"));
+    println!("Part 2: {}", exterior_surface_area("input.txt"));
+}
+
+fn generate_map(file: &str, map: &mut Vec<Vec<Vec<bool>>>) {
     let contents = fs::read_to_string(file).expect("Error reading file");
 
     let mut coordinates: Vec<[usize; 3]> = vec!();
@@ -28,8 +34,6 @@ fn surface_area(file: &str) -> usize {
         coordinates.push(next_x_y_z);
     }
 
-    let mut map: Vec<Vec<Vec<bool>>> = vec!();
-
     for _ in 0..=max_x_y_z[0] + 1 {
         let mut temp_2: Vec<Vec<bool>> = vec!();
         for _ in 0..=max_x_y_z[1] + 1 {
@@ -45,7 +49,9 @@ fn surface_area(file: &str) -> usize {
     for cube in coordinates {
         map[cube[0]][cube[1]][cube[2]] = true;
     }
+}
 
+fn count_surface_area(map: &Vec<Vec<Vec<bool>>>) -> usize {
     let mut surface_area: usize = 0;
 
     for z in 0..map.len() - 1 {
@@ -84,4 +90,140 @@ fn surface_area(file: &str) -> usize {
     }
 
     return surface_area;
+}
+
+fn surface_area(file: &str) -> usize {
+    let mut map: Vec<Vec<Vec<bool>>> = vec!();
+    generate_map(file, &mut map);
+
+    return count_surface_area(&map);
+}
+
+fn exterior_surface_area(file: &str) -> usize {
+    let mut map: Vec<Vec<Vec<bool>>> = vec!();
+    generate_map(file, &mut map);
+    fill_interiors(&mut map);
+
+    return count_surface_area(&map);
+}
+
+fn fill_interiors(map: &mut Vec<Vec<Vec<bool>>>) {
+    let mut checked: Vec<Vec<Vec<bool>>> = vec!();
+
+    for z in 0..map.len() {
+        let mut temp_2: Vec<Vec<bool>> = vec!();
+        for y in 0..map[z].len() {
+            let mut temp_1: Vec<bool> = vec!();
+            for x in 0..map[z][y].len() {
+                temp_1.push(map[z][y][x]);
+            }
+            temp_2.push(temp_1);
+        }
+        checked.push(temp_2);
+    }
+
+    for z in 0..map.len() {
+        for y in 0..map[z].len() {
+            for x in 0..map[z][y].len() {
+                if !checked[z][y][x] {
+                    let (neighbours, interior) = flood(&map, z, y, x);
+                    if interior {
+                        for (z, y, x) in neighbours {
+                            map[z][y][x] = true;
+                            checked[z][y][x] = true;
+                        }
+                    } else {
+                        for (z, y, x) in neighbours {
+                            checked[z][y][x] = true;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+fn flood(
+    map: &Vec<Vec<Vec<bool>>>,
+    z: usize,
+    y: usize,
+    x: usize
+) -> (Vec<(usize, usize, usize)>, bool) {
+    let mut neighbours: Vec<(usize, usize, usize)> = vec![(z, y, x)];
+    let is_interior = recur(&map, &mut neighbours, z, y, x);
+    return (neighbours, is_interior);
+}
+
+fn recur(
+    map: &Vec<Vec<Vec<bool>>>,
+    neighbours: &mut Vec<(usize, usize, usize)>,
+    z: usize,
+    y: usize,
+    x: usize
+) -> bool {
+    if map[z][y][x] {
+        return true;
+    }
+    let mut is_interior = true;
+    if z < map.len() - 1 {
+        if !neighbours.contains(&(z + 1, y, x)) {
+            neighbours.push((z + 1, y, x));
+            if !recur(&map, neighbours, z + 1, y, x) {
+                is_interior = false;
+            }
+        }
+    }
+    if y < map[z].len() - 1 {
+        if !neighbours.contains(&(z, y + 1, x)) {
+            neighbours.push((z, y + 1, x));
+            if !recur(&map, neighbours, z, y + 1, x) {
+                is_interior = false;
+            }
+        }
+    }
+    if x < map[z][y].len() - 1 {
+        if !neighbours.contains(&(z, y, x + 1)) {
+            neighbours.push((z, y, x + 1));
+            if !recur(&map, neighbours, z, y, x + 1) {
+                is_interior = false;
+            }
+        }
+    }
+    if z > 0 {
+        if !neighbours.contains(&(z - 1, y, x)) {
+            neighbours.push((z - 1, y, x));
+            if !recur(&map, neighbours, z - 1, y, x) {
+                is_interior = false;
+            }
+        }
+    }
+    if y > 0 {
+        if !neighbours.contains(&(z, y - 1, x)) {
+            neighbours.push((z, y - 1, x));
+            if !recur(&map, neighbours, z, y - 1, x) {
+                is_interior = false;
+            }
+        }
+    }
+    if x > 0 {
+        if !neighbours.contains(&(z, y, x - 1)) {
+            neighbours.push((z, y, x - 1));
+            if !recur(&map, neighbours, z, y, x - 1) {
+                is_interior = false;
+            }
+        }
+    }
+
+    if
+        z == 0 ||
+        y == 0 ||
+        x == 0 ||
+        z == map.len() - 1 ||
+        y == map[z].len() - 1 ||
+        x == map[z][y].len() - 1
+    {
+        is_interior = false;
+    }
+
+    return is_interior;
 }
